8a2b5fd589ec13e20408e3127dc2e4dedcf05bba
Andrea Mistrali <andrea@e-tainment.com.mt>
2017-09-28 07:24:51 +0200

    cleanup, maybe restarting

---
+++ README.md
@@ -0,0 +1,15 @@
+# SJCAM API ##
+http://zsub.github.io/SJCam-API/
+
+# SJCAM module
+https://github.com/AdamLaurie/sjcam.git
+_this is underlined_
+
+Sample XML:
+    <?xml version="1.0" encoding="UTF-8" ?>
+    <Function>
+        <Cmd>2003</Cmd>
+        <Status>0</Status>
+    </Function>
+
+
--- sjcam.py
+++ sjcam.py
@@ -1,31 +1,32 @@
-# import httplib
+"""
+SJCAM camera library
+Send command to the camera, via WIFI, to configure it and to read parameters
 
+Basic functions are:
+- sync camera date/time with the computer
+- load presets of settings to the camera
+- download media files from camera
 """
-"""
+import simpleyaml as yaml
+import requests
 
 
 class sjcamCommand(object):
     """command for sjcam4000"""
     def __init__(self, id, description="unknown", parameters=[],
-            readOnly=False, writeOnly=False, format=""):
+            readOnly=False, format=""):
 
         super(sjcamCommand, self).__init__()
-        self.id = id
+        self.id = int(id)
         self.description = description
         self.parameters = parameters
         self.readOnly = readOnly
-        self.writeOnly = writeOnly
         self.format = format
 
-    @property
-    def fn(self):
-        """
-        Return function name
-        """
         fnParts = self.description.split(' ')
         fnParts[0] = fnParts[0].lower()
         fnParts[1:] = [f.capitalize() for f in fnParts[1:]]
-        return ''.join(fnParts)
+        self.fn = ''.join(fnParts)
 
     def paramInt(self, paramStr):
         """
@@ -45,13 +46,11 @@ class sjcamCommand(object):
         return None
 
     def __repr__(self):
-
         return r"<%s(id=%d, description='%s')>" % (self.__class__.__name__,
             self.id,
             self.description)
 
     def __str__(self):
-
         return "%d: %s" % (self.id,
             self.description)
 
@@ -60,22 +59,86 @@ class sjcam(object):
     """sjcam connection library"""
 
     address = "192.168.1.254"
-    url = "http://%s" % address
-    cmdUrl = "%s/?custom=1&cmd=%%d&par=%%d" % url
+    url = "http://%s/" % address
+    cmdUrl = "%s?custom=1&cmd=%%d&par=%%d" % url
     rtsp = "rtsp://%s/sjcam.mov" % address
 
     def __init__(self):
         super(sjcam, self).__init__()
+        self.commands = []
         print "parsing yaml"
-        yaml = open('sjcam.yaml', 'r')
-        commands = yaml.load(yaml)
+        with open('sjcam.yaml', 'r') as yamlFile:
+            commands = yaml.load(yamlFile)
+            print commands
+        for id, values in commands['commands'].items():
+            self.commands.append(sjcamCommand(id=id,
+                description=values.get('description'),
+                parameters=values.get('parameter', []),
+                readOnly=values.get('readOnly', False),
+                format=values.get('format', '')))
 
-    def httpCall(self, method, cmdId, parameter):
-        data = {"custom": 1, "cmd": cmdId, "par": parameter}
-        r = requests.request(method=method, url=self.url, data=data )
-        if r.ok:
-            print r.reason
+    def getCommand(self, command, parameter=None):
+        """
+        Prepare a command to execute
+        """
+        cmd = [c for c in self.commands if c.description == command or c.fn == command]  # NOQA
+        if cmd > 0:
+            cmd = cmd[0]
+            if parameter in cmd.parameters:
+                paramId = cmd.parameters.index(parameter)
+            else:
+                paramId = None
+
+    def httpCall(self, method="GET", cmdId=None, paramId=None):
+        """
+        Generic method to do an HTTP call
+        @cmdId - numeric id of the command to issue
+        @parameter - numeric id of the parameter
+        """
+
+        if cmdId is not None:
+            data = {"custom": 1, "cmd": cmdId}  # , "par": parameter}
+        if paramId is not None:
+            data["par"] = paramId
+        print method, self.url, data
+        try:
+            r = requests.request(method=method, url=self.url,
+                data=data, timeout=5)
+        except requests.exceptions.ConnectTimeout:
+            return None
+        return self.parseResponse(r)
+
+    def httpPing(self):
+        """
+        Check if camera is available
+        """
+        r = self.httpCall(method='HEAD')
+        print r
+        if r is not None:
+            return r.ok
+
+    def parseResponse(self, response):
+        """
+        <?xml version="1.0" encoding="UTF-8" ?>
+        <Function>
+            <Cmd>2003</Cmd>
+            <Status>0</Status>
+        </Function>
+        returns
+        {'Cmd': 2003, 'Status': 0}
+        """
+        root = ET.fromstring(response)
+        response = {}
+        for child in root:
+            response[child.tag] = child.text
+        return response
+
 
 if __name__ == "__main__":
     print "SJcam lib"
     cam = sjcam()
+    # print "Ping"
+    # if cam.httpPing():
+    #     print "Camera available"
+    # else:
+    #     print "Check connection to camera"
